b999b5d8527593ba4f3dbf730a61a2ab99fae0d0
itbienvenu <bienvenugashema@gmail.com>
2025-07-30 13:37:13 +0200

    Creating the starting blocks

---
+++ 1.py
@@ -0,0 +1,12 @@
+from locs.data import (
+    get_provinces,
+    get_districts_from_province,
+    get_sectors_from_district,
+    get_cells_from_sector,
+    get_villages_from_cell,
+    get_all_children
+)
+provinces = get_provinces()
+print(provinces)
+# districts = get_districts_from_province('Kigali')
+# print(districts)
